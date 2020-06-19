cb58f46fb52cf2d585961857a20802676d69490a
Creigh Battle <creighbattle@gmail.com>
2020-06-19 18:04:48 -0400

    Updates

---
+++ precache-manifest.55f4c947bb105aa6f28d5f5bd10e48f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb8de6f94195a7dc55f14fe865f4f88d",
+    "url": "/speedy/index.html"
+  },
+  {
+    "revision": "bc85e0a285d472f6b7a0",
+    "url": "/speedy/static/css/main.4394214b.chunk.css"
+  },
+  {
+    "revision": "cd4c31bfbe4685b06f3f",
+    "url": "/speedy/static/js/2.eb11a0b8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/speedy/static/js/2.eb11a0b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc85e0a285d472f6b7a0",
+    "url": "/speedy/static/js/main.97793fb3.chunk.js"
+  },
+  {
+    "revision": "bf5416747eb7cf7375ed",
+    "url": "/speedy/static/js/runtime-main.e3dae068.js"
+  }
+]);
